1c4dd8650a36b7c34794b77b28154ab08fd071ef
Alma Linnea Hansen <almal@rackham3.uppmax.uu.se>
2022-05-11 13:13:57 +0200

    changed run file

--- RNA_preprocess/run_trimming.sh
+++ RNA_preprocess/run_trimming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH -A uppmax2022-2-5
-#SBATCH --reservation=uppmax2022-2-5_3
+#SBATCH --reservation=uppmax2022-2-5_8
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
@@ -13,26 +13,32 @@ module load bioinfo-tools
 module load trimmomatic
 # Your commands
 trimmomatic PE -threads 2 -phred33 \
+-basein /home/almal/GenomeAnalysis_Alma/RNA_raw_data/ERR2036629_1.fastq.gz \
+-baseout /home/almal/GenomeAnalysis_Alma/RNA_preprocess/ERR2036629_Trimmed.fastq.gz \
+ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.36/snowy/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads \
+LEADING:20 TRAILING:20 SLIDINGWINDOW:1:3 MINLEN:40 MAXINFO:40:0.5
+
+trimmomatic PE -threads 2 -phred33 \
 -basein /home/almal/GenomeAnalysis_Alma/RNA_raw_data/ERR2036630_1.fastq.gz \
 -baseout /home/almal/GenomeAnalysis_Alma/RNA_preprocess/ERR2036630_Trimmed.fastq.gz \
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 \
+ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.36/snowy/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:1:3 MINLEN:40 MAXINFO:40:0.5
 
 trimmomatic PE -threads 2 -phred33 \
 -basein /home/almal/GenomeAnalysis_Alma/RNA_raw_data/ERR2036631_1.fastq.gz \
 -baseout /home/almal/GenomeAnalysis_Alma/RNA_preprocess/ERR2036631_Trimmed.fastq.gz \
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 \
+ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.36/snowy/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:1:3 MINLEN:40 MAXINFO:40:0.5
 
 trimmomatic PE -threads 2 -phred33 \
 -basein /home/almal/GenomeAnalysis_Alma/RNA_raw_data/ERR2036632_1.fastq.gz \
 -baseout /home/almal/GenomeAnalysis_Alma/RNA_preprocess/ERR2036632_Trimmed.fastq.gz \
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 \
+ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.36/snowy/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:1:3 MINLEN:40 MAXINFO:40:0.5
 
 trimmomatic PE -threads 2 -phred33 \
 -basein /home/almal/GenomeAnalysis_Alma/RNA_raw_data/ERR2036633_1.fastq.gz \
 -baseout /home/almal/GenomeAnalysis_Alma/RNA_preprocess/ERR2036633_Trimmed.fastq.gz \
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 \
+ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.36/snowy/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads \
 LEADING:20 TRAILING:20 SLIDINGWINDOW:1:3 MINLEN:40 MAXINFO:40:0.5
 
