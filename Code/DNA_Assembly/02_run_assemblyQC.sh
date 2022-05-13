c1991ebe6e9aad2ab0470397f5b6ae968366894b
AndraAlma <95489040+AndraAlma@users.noreply.github.com>
2022-05-13 14:05:41 +0200

    updated runfile QA

--- Code/DNA_Assembly/02_run_assemblyQC.sh
+++ Code/DNA_Assembly/02_run_assemblyQC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH -A uppmax2022-2-5
-#SBATCH --reservation=uppmax2022-2-5_3
+#SBATCH --reservation=uppmax2022-2-5_11
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
@@ -11,6 +11,9 @@
 # Load modules
 module load bioinfo-tools
 module load MUMmer
+module load quast
 # Your commands
-mummer -mum -b -c /home/almal/GenomeAnalysis_Alma/reference/* /home/almal/GenomeAnalysis_Alma/DNA_Assembly/assembly_L_Ferriphilum.contigs.fasta > ref_qry.mums
-mummerplot --postscript --prefix=ref_qry ref_qry.mums
+mummer -mum -b -c /home/almal/GenomeAnalysis_Alma/DNA_Assembly/assembly_L_Ferriphilum.contigs.fasta /home/almal/GenomeAnalysis_Alma/reference/* > DNAQA2.mums
+mummerplot -png -p DNAQA2 DNAQA2.mums
+
+quast.py -R /home/almal/GenomeAnalysis_Alma/reference/* --threads 2 /home/almal/GenomeAnalysis_Alma/DNA_Assembly/assembly_L_Ferriphilum.contigs.fasta 
