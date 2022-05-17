239e79ce3065a4a72fa205001bc129b4a1800878
AndraAlma <95489040+AndraAlma@users.noreply.github.com>
2022-05-17 15:23:09 +0200

    updated r script

--- Code/Differential_Expression/09_run_DESeq.R
+++ Code/Differential_Expression/09_run_DESeq.R
@@ -1,3 +1,15 @@
+#To load in all the correct stuff:
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("ReportingTools")
+BiocManager::install("DESeq2")
+
+library("DESeq2")
+library("ReportingTools")
+
+
 #The conditions for each of the 5 samples into a 5x1 data frame
 condition_vector <- data.frame('continuous', 'continuous', 'batch', 'batch', 'continuous')
 new_vector <- data.frame(t(condition_vector))
@@ -6,12 +18,9 @@ new_vector <- data.frame(t(condition_vector))
 sample <- read.table("mapped_read_counts.txt", header = FALSE)
 sample_file <- sample[,-1]
 
-#directory <- "C:/Users/dansa/Documents/assignments_Alma_Hansen/Genome analysis/"
-library("DESeq2")
-#sampleTable <- data.frame(sampleName = sample_file, fileName = sample_file, condition = condition_vector)
-#sample_file$new_vector <- factor(sample_file$new_vector)
 
-#Set column names to avoid error messages
+
+#Set column names to avoid some error messages
 colnames(new_vector) <- "condition"
 colnames(sample_file) <- NULL
 
@@ -30,20 +39,16 @@ res05 <- results(dds, alpha=0.05)
 summary(res05)
 sum(res05$padj < 0.05, na.rm=TRUE)
 
-#?
-resLFC <- lfcShrink(dds, coef="condition_Continous_vs_Batch", type="apeglm")
 
-# Plot ??
-plotMA(res, ylim=c(-2,2))
+# Plot MA
+plotMA(res05, ylim=c(-4,4))
 
-# Plot ??
-plotCounts(dds, gene=which.min(res$padj), intgroup="condition")
+# Plot PCA
+x = vst(ddsHTSeq)
+plotPCA(x, intgroup="condition")
 
-library("ReportingTools")
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
 
-BiocManager::install("ReportingTools")
+#Create report with all CDS
 report <- HTMLReport(shortName = 'DiffExp_analysis', title = 'Differential expression analysis on continous vs batch cultures with gene names', reportDirectory = ".")
 publish(ddsHTSeq, report, pvalueCutoff=0.05, make.plots = TRUE, factor = sampleTable$condition, reportDir = ".")
 finish(report)
